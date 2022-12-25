c9a7882eab4867f97419f8f234c20daa535594c1
arne_van_bavel <arne.vanbavel@protime.eu>
2022-12-25 15:21:09 +0100

    start of php lesson

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>HTML 5 Boilerplate</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <ol>
+    <a href="session-1/1-hello-world.php">
+      <li>Hello World</li>
+    </a>
+  </ol>
+</body>
+
+</html>
---
+++ session-1/1-hello-world.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>HTML 5 Boilerplate</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>
+    <?php echo "Hellow World"; ?>
+  </h1>
+
+  <p>
+    <?php echo "This is a text as homework"; ?> </p>
+</body>
+
+</html>
